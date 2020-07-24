01efc6c95673bd1872af3aadd3fc4ee354c450f4
ritwikjamuar <ritwikjamuar@gmail.com>
2020-07-24 23:56:55 +0530

    Added code to set-up the DataBinding and ViewModel in MainActivity.kt.

--- app/src/main/java/demo/ritwik/endless/ui/activity/MainActivity.kt
+++ app/src/main/java/demo/ritwik/endless/ui/activity/MainActivity.kt
@@ -1,16 +1,70 @@
 package demo.ritwik.endless.ui.activity
 
+import android.os.Bundle
+
 import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+
+import androidx.lifecycle.ViewModelProvider
 
 import demo.ritwik.endless.R
 
+import demo.ritwik.endless.databinding.ActivityMainBinding
+
+import demo.ritwik.endless.mvvm.repository.MainRepository
+
+import demo.ritwik.endless.mvvm.viewModel.MainVMFactory
+import demo.ritwik.endless.mvvm.viewModel.MainViewModel
+
+/**
+ * [android.app.Activity] to show [List] of MotoGP Riders.
+ *
+ * @author Ritwik Jamuar
+ */
 class MainActivity : AppCompatActivity() {
 
+	/*---------------------------------------- Components ----------------------------------------*/
+
+	/**[androidx.databinding.ViewDataBinding] reference for this [android.app.Activity].*/
+	private lateinit var binding : ActivityMainBinding
+
+	/**Reference of [MainViewModel].*/
+	private lateinit var viewModel : MainViewModel
+
+	/*------------------------------------ Activity Callbacks ------------------------------------*/
+
 	override fun onCreate(savedInstanceState : Bundle?) {
+
+		inject()
+
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+		initializeViews()
+
 	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		cleanUp()
+	}
+
+	/*-------------------------------------- Private Methods -------------------------------------*/
+
+
+	/**Fulfill the required dependencies of this [android.app.Activity].*/
+	private fun inject() {
+		viewModel =
+			ViewModelProvider(this, MainVMFactory(MainRepository(this.applicationContext)))
+				.get(MainViewModel::class.java)
+	}
+
+	/**Initialize the [android.view.View]s under [ActivityMainBinding].*/
+	private fun initializeViews() = Unit
+
+	/**De-references any references to avoid a potential Memory Leak.*/
+	private fun cleanUp() = Unit
 
 }
